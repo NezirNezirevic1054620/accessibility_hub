d70cea50e0ec6a7a77c2ff781ed9fdabcfa45dec
NezirNezirevic1054620 <1054620@hr.nl>
2024-09-22 21:19:16 +0200

    init 2

--- accessibility_hub/accessibility_hub/settings.py
+++ accessibility_hub/accessibility_hub/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-l99^%blma&#$&rntshpt#5n9c#l9reqi!n66o@pmrg%)6m@oc#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- accessibility_hub/admin_hub/utils.py
+++ accessibility_hub/admin_hub/utils.py
@@ -1,4 +1,4 @@
-# import random
+import random
 import secrets
 import string
 
@@ -7,7 +7,7 @@ def username_generator(full_name):
     if len(full_name) > 1:
         first_letter = full_name[0][0]
         three_letters_surname = full_name[-1][:3].rjust(3, "x")
-        number = "{:03d}".format(secrets.randbelow(999) + 1)
+        number = "{:03d}".format(random.randrange(1, 999))
         return "{}{}{}".format(first_letter, three_letters_surname, number)
 
     else:
